746c4ab4d8ad6a7f83680719eca2c1eecf450cf9
Arash Abbasi Larki <arash.abbasi20@gmail.com>
2024-05-19 17:10:01 +0330

    telegram_bot_v0.1.6

--- telegram_bot_v0.1.py
+++ telegram_bot_v0.1.py
@@ -70,14 +70,17 @@ EMAIL = 100
 
 execution_counter = 0
 IMG_IDX_COUNTER = 0
-current_index = 0 
+# current_index = 0 
+first_time_loop = True
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     # global air_drop_counter
     # air_drop_counter = 0
-
+    global current_index, first_time_loop
+    current_index = 0  # Reset current_index to 0
+    first_time_loop = True
     tel_user_id = update.effective_user.id
 
     if tel_user_id in data_base['tel_user_id'].values:
@@ -102,6 +105,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         await update.message.reply_text(text=print_txt, reply_markup=reply_markup)
     elif update.callback_query:
         query = update.callback_query
+        current_index = 0 
         await query.answer()
         await query.edit_message_text(text=print_txt, reply_markup=reply_markup)
 
@@ -222,37 +226,116 @@ async def global_exchange(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
+
+    # Reset current_index when main_menu is called
+    global current_index, first_time_loop
+    current_index = 0
+    first_time_loop = True
+
     return await start_over(update, context)
 
 
+###############################################################################
+
+# async def send_airdrops_album(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+#     query = update.callback_query
+#     await query.answer()
+    
+#     global current_index
+
+#     # Get the index from the callback data
+#     if query.data.isdigit():
+#         current_index = int(query.data)
+#     else:
+#         current_index = 0
 
 
+
+#     print('---')
+#     print(update.callback_query.data)
+#     print('---')
+
+#     print('***')
+#     print(update.callback_query)
+#     print('***')
+
+#     if update.callback_query and update.callback_query.data:
+#         print('exeasagsg')
+#         print(update.callback_query.data)
+#         print(update.callback_query.data.split("_")[-1])
+
+#     # List of photo file_ids or URLs
+#     photo_list = [
+#         'https://th.bing.com/th/id/OIP.TzP2op3lkhlTh6oOHamacAHaHa?rs=1&pid=ImgDetMain',
+#         'https://th.bing.com/th/id/OIP.RH2gc-Oe1qSvCjD3IRYAyQHaE7?rs=1&pid=ImgDetMain',
+#         'https://th.bing.com/th/id/OIP.AW8VfeeCp9v_xzlVdciPpAHaEo?rs=1&pid=ImgDetMain',
+#         'https://th.bing.com/th/id/OIP.R3U06JEJvoROC7iFM1AnzAHaEK?rs=1&pid=ImgDetMain'
+#     ]
+
+#     # # Determine the current index
+#     # if update.callback_query and update.callback_query.data:
+#     #     current_index = int(update.callback_query.data.split("_")[-1])
+#     # else:
+#     #     current_index = 0
+
+#     # Ensure current_index stays within the bounds of photo_list
+#     current_index = max(0, min(current_index, len(photo_list) - 1))
+
+#     # # If "Finish" button is clicked, show the main menu
+#     # if update.callback_query and update.callback_query.data == "FINISH":
+#     #     await main_menu(update, context)
+#     #     return START_ROUTES
+
+#     # Construct caption with current index and total number of photos
+#     caption = f"{current_index + 1} out of {len(photo_list)}"
+
+#     # Construct InlineKeyboardMarkup based on current message index
+#     buttons = []
+#     if current_index == 0:
+#         buttons.append([InlineKeyboardButton("Next", callback_data=str(current_index + 1)),
+#                         InlineKeyboardButton("Back to Main Menu", callback_data=str(MAIN_MENU))])
+#     elif current_index == len(photo_list) - 1:
+#         buttons.append([InlineKeyboardButton("Finish", callback_data=str(MAIN_MENU)),
+#                         InlineKeyboardButton("Previous", callback_data=str(current_index - 1))])
+#     else:
+#         buttons.append([InlineKeyboardButton("Next", callback_data=str(current_index + 1)),
+#                         InlineKeyboardButton("Previous", callback_data=str(current_index - 1))])
+
+#     reply_markup = InlineKeyboardMarkup(buttons)
+#     print(current_index)
+#     # Send the current photo with caption and navigation buttons
+#     await context.bot.send_photo(
+#         chat_id=update.effective_chat.id,
+#         photo=photo_list[current_index],
+#         caption=caption,
+#         reply_markup=reply_markup
+#     )
+
+#     return SEND_IMG
+
+###############################################################################
 async def send_airdrops_album(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
-    
-    global current_index
+
+    global current_index, first_time_loop
+
+    print(f"it is digit: {current_index}")
+
+    # # Get the index from the callback data
+    # if query.data.isdigit():
+    #     current_index = int(query.data)
+    #     print(f"it is digit: {current_index}")
+
 
     # Get the index from the callback data
-    if query.data.isdigit():
-        current_index = int(query.data)
-    else:
+    if query.data.isdigit() and first_time_loop:
         current_index = 0
-
-
-
-    print('---')
-    print(update.callback_query.data)
-    print('---')
-
-    print('***')
-    print(update.callback_query)
-    print('***')
-
-    if update.callback_query and update.callback_query.data:
-        print('exeasagsg')
-        print(update.callback_query.data)
-        print(update.callback_query.data.split("_")[-1])
+        first_time_loop = False
+        print(f"it is first time: {current_index}")
+    else: 
+        current_index = int(query.data)
+        print(f"it is not first time: {current_index}")
 
     # List of photo file_ids or URLs
     photo_list = [
@@ -262,19 +345,8 @@ async def send_airdrops_album(update: Update, context: ContextTypes.DEFAULT_TYPE
         'https://th.bing.com/th/id/OIP.R3U06JEJvoROC7iFM1AnzAHaEK?rs=1&pid=ImgDetMain'
     ]
 
-    # # Determine the current index
-    # if update.callback_query and update.callback_query.data:
-    #     current_index = int(update.callback_query.data.split("_")[-1])
-    # else:
-    #     current_index = 0
-
     # Ensure current_index stays within the bounds of photo_list
     current_index = max(0, min(current_index, len(photo_list) - 1))
-
-    # # If "Finish" button is clicked, show the main menu
-    # if update.callback_query and update.callback_query.data == "FINISH":
-    #     await main_menu(update, context)
-    #     return START_ROUTES
 
     # Construct caption with current index and total number of photos
     caption = f"{current_index + 1} out of {len(photo_list)}"
@@ -292,7 +364,7 @@ async def send_airdrops_album(update: Update, context: ContextTypes.DEFAULT_TYPE
                         InlineKeyboardButton("Previous", callback_data=str(current_index - 1))])
 
     reply_markup = InlineKeyboardMarkup(buttons)
-    print(current_index)
+
     # Send the current photo with caption and navigation buttons
     await context.bot.send_photo(
         chat_id=update.effective_chat.id,
@@ -302,8 +374,6 @@ async def send_airdrops_album(update: Update, context: ContextTypes.DEFAULT_TYPE
     )
 
     return SEND_IMG
-
-
 
 
 
